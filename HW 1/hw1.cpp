1feac2e83f856976d54b89e25d7f199b7992b164
smidge1k <37225113+smidge1k@users.noreply.github.com>
2018-03-10 12:47:03 -0600

    Add files via upload

---
+++ HW 1/hw1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+/**
+* @Author: Darrian Johnson
+* @email: dmj0018@auburn.edu
+* 1-20-2018
+* hw1.cpp
+* 
+* Compiled using emacs and g++ compilier on Linux machine in Engineering Lab via Putty from Windows 
+* (syllabus method).
+*/
+
+
+int main()
+{
+   const double MY_FRAC = 0.001;
+   double mousesweet;
+   double mouseweight;
+   double humanweight;
+   double humansweet;
+   double sodapop;
+
+   cout << "What is the amount of artificial sweetener needed to kill a mouse: \n";
+   cin >> mousesweet;
+
+   cout << "\nWhat is the weight of the mouse: ";
+   cin >> mouseweight;
+
+   cout << "\nWhat is the weight of the dieter: ";
+   cin >> humanweight;
+
+   humansweet = (mousesweet / mouseweight) * humanweight;
+   sodapop = (humansweet / MY_FRAC);
+
+   cout << "\nYou can drink " << sodapop << " diet soda without dying as a result." << endl;
+
+   system("pause");
+   return 0;
+}
